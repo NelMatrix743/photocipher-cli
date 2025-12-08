fdfb5730128c1dd6d6e074ea093fac5355f89296
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-08 10:49:29 +0000

    refactor(db): change query variable names

--- src/photocipher/db/queries.py
+++ src/photocipher/db/queries.py
@@ -10,7 +10,7 @@ from const import DB_TABLE_NAME
 
 # CRD -> Create, Read, Delete (no Update action)
 
-table_creation_query: str = f"""
+TABLE_CREATION_QUERY: str = f"""
     CREATE TABLE IF NOT EXISTS {DB_TABLE_NAME} (
         eci_entry_id text NOT NULL PRIMARY KEY,
         cryptographic_key text NOT NULL,
@@ -22,25 +22,24 @@ table_creation_query: str = f"""
 """
 
 
-table_entry_insert_query: str = f"""
-    INSERT INTO {DB_TABLE_NAME}(
+ENTRY_INSERTION_QUERY: str = f"""
+    INSERT INTO {DB_TABLE_NAME} (
         eci_entry_id,
         cryptographic_key, 
         img_filename, 
         en_img_filename, 
         date, 
         time          
-        )
-    VALUES(?, ?, ?, ?, ?, ?);
+    ) VALUES(?, ?, ?, ?, ?, ?);
 """
 
 
-table_entry_retrieval_query: str = f"""
+ENTRY_RETRIEVAL_QUERY: str = f"""
     SELECT * FROM {DB_TABLE_NAME} WHERE eci_entry_id = ?;
 """
 
 
-table_entry_delete_entry: str = f"""
+ENTRY_DELETION_QUERY: str = f"""
     DELETE FROM {DB_TABLE_NAME} WHERE eci_entry_id = ?;
 """
 
