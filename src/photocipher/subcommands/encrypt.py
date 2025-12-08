c8e8b68660684ce96f3ea301c8bbed872194a0f7
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-08 08:30:22 +0000

    refactor: slight refactoring

---
+++ src/photocipher/subcommands/encrypt.py
@@ -0,0 +1,6 @@
+###############################################################################
+# Programmer.name = Nelson Chidi                                              #
+# Programmer.nick_name = Nelmatrix                                            #
+# Programmer.GitHub.user_name = NelMatrix743                                  #
+# Programmer.GitHub.url = https://github.com/NelMatrix743                     #
+###############################################################################
