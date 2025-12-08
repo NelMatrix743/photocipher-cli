95119a940ebf9f4586f789f2ab973399246cd29b
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-08 13:58:54 +0000

    feat: add database directory name constant

--- src/photocipher/const.py
+++ src/photocipher/const.py
@@ -5,11 +5,16 @@
 # Programmer.GitHub.url = https://github.com/NelMatrix743                     #
 ###############################################################################
 
-
 # app-specific metadata
 APP_NAME: str = "photocipher"
 APP_DEVELOPER: str = "nelmatrix"
 
 
-# database metadata
-DB_NAME: str = ""
+# database-specific metadata
+# ECI -> Encrypted Image
+DB_FILE_NAME: str = "ECI_METADATA.db"
+DB_DIR_NAME: str = "db"
+DB_TABLE_NAME: str = "encrypted_images_table"
+
+
+# ECI-specific metadata
