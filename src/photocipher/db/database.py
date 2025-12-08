d21ce8a6d0ee47fee4354cf4f85d4d8fe20e64f7
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-08 14:25:13 +0000

    feat: remove init_db() && move database init operation into __init__

--- src/photocipher/db/database.py
+++ src/photocipher/db/database.py
@@ -5,18 +5,46 @@
 # Programmer.GitHub.url = https://github.com/NelMatrix743                     #
 ###############################################################################
 
+
 import sqlite3 as sql
-from const import DB_FILE_NAME, DB_TABLE_NAME
+from paths import AppPathsManager
+from queries import (
+    TABLE_CREATION_QUERY,
+    ENTRY_INSERTION_QUERY,
+    ENTRY_RETRIEVAL_QUERY,
+    ENTRY_DELETION_QUERY
+)
+
 
 class DB:
 
-    def __init__(self) -> None:
+    def __init__(self, db_path: str) -> None:
+        self.db_path: str = db_path
+
+        # init DB, commit, && close connection
+        with sql.connect(self.db_path) as db_connect:
+            self.db_cursor: sql.Cursor = db_connect.cursor()
+
+            # create table
+            self.db_cursor.execute(TABLE_CREATION_QUERY)
+            db_connect.commit()
+
+
+    def insert(self) -> bool:
+        pass
+
+
+    def retrieve(self) -> bool:
+        pass
+
+
+    def delete(self) -> bool:
         pass
 
 
 
 # export a general interface
 
-db: DB = DB()
+db: DB = DB(AppPathsManager.get_db_path())
 
 # eosc
