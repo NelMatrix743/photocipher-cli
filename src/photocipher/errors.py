8f8f3e5ee89decbce727b17c2acf1331b4ecf490
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-08 16:48:56 +0000

    feat(db): implement custom exception classes for handling relevant db errors

--- src/photocipher/errors.py
+++ src/photocipher/errors.py
@@ -5,3 +5,24 @@
 # Programmer.GitHub.url = https://github.com/NelMatrix743                     #
 ###############################################################################
 
+
+class ENTRY_EXISTS(Exception):
+    
+    def __init__(self) -> None:
+        self.message: str = "[ERROR] Entry exists in the database"
+        super().__init__(self.message)
+
+
+class ENTRY_NOT_FOUND(Exception):
+    
+    def __init__(self) -> None:
+        self.message: str = "[ERROR] Entry could not be retrieved"
+        super().__init__(self.message)
+
+
+
+# export public ones
+
+__all__: list[str] = ["ENTRY_EXISTS", "ENTRY_NOT_FOUND"]
+
+# eosc
